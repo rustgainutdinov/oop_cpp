b5807ad0f800548ec1e5b5c25d271746b21c69e4
Rustam <rustamikc@gmail.com>
2021-02-15 07:21:28 +0300

    Initial Commit

---
+++ oop/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  oop
+//
+//  Created by Рустам Гайнутдинов on 15.02.2021.
+//  Copyright © 2021 Рустам Гайнутдинов. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
